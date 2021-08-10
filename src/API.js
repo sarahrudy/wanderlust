a287c62cfdcc0302074375b1fb042f49cf6545cc
Sarah <sarahrudy@gmail.com>
2021-08-09 20:28:20 -0600

    Add API fetch calls to fetch data

--- src/API.js
+++ src/API.js
@@ -1,1 +1,21 @@
 // https://codesandbox.io/s/network-request-practice-forked-with-changes-cfs77
+
+export function getTravelerData() {
+  fetch("http://localhost:3001/api/v1/travelers")
+    .then(response => checkForError(response))
+    .catch(err => console.log(`Traveler API Error: ${err.message}`))
+}
+
+export function getTraveler(id) {
+  fetch(`http://localhost:3001/api/v1/travelers/${id}`)
+    .then(response => checkForError(response))
+    .catch(err => console.log(`Traveler API Error: ${err.message}`))
+}
+
+export function getTripsData() {
+  fetch("http://localhost:3001/api/v1/trips")
+    .then(response => checkForError(response))
+    .catch(err => console.log(`Trips API Error: ${err.message}`))
+}
+
+// add destinationsData 
