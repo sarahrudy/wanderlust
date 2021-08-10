7cdff14b8b1cd79d145301be938631ba4180ee6d
Sarah <sarahrudy@gmail.com>
2021-08-09 23:07:41 -0600

    Add 'return' in front of all fetch calls

--- src/API.js
+++ src/API.js
@@ -1,25 +1,25 @@
 // https://codesandbox.io/s/network-request-practice-forked-with-changes-cfs77
 
-export function getTravelerData() {
-  fetch("http://localhost:3001/api/v1/travelers")
-    .then(response => checkForError(response))
-    .catch(err => console.log(`Traveler API Error: ${err.message}`))
-}
 
 export function getTraveler(id) {
-  fetch(`http://localhost:3001/api/v1/travelers/${id}`)
+  return fetch(`http://localhost:3001/api/v1/travelers/${id}`)
+  .then(response => checkForError(response))
+  .catch(err => console.log(`Traveler API Error: ${err.message}`))
+}
+export function getAllTravelersData() {
+  return fetch("http://localhost:3001/api/v1/travelers")
     .then(response => checkForError(response))
     .catch(err => console.log(`Traveler API Error: ${err.message}`))
 }
 
-export function getTripsData() {
-  fetch("http://localhost:3001/api/v1/trips")
+export function getAllTripsData() {
+  return fetch("http://localhost:3001/api/v1/trips")
     .then(response => checkForError(response))
     .catch(err => console.log(`Trips API Error: ${err.message}`))
 }
 
-export function getDestinationsData() {
-  fetch("http://localhost:3001/api/v1/destinations")
+export function getAllDestinationsData() {
+  return fetch("http://localhost:3001/api/v1/destinations")
     .then(response => checkForError(response))
     .catch(err => console.log(`Destinations API Error: ${err.message}`))
 }
