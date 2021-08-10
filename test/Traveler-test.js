cc4535e26f692104d2eccd50f20d31f054ede393
Sarah <sarahrudy@gmail.com>
2021-08-09 18:23:39 -0600

    Include traveler variable and instantiate a new instance of Traveler

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,13 +1,15 @@
 import chai from 'chai';
 const expect = chai.expect;
-import Traveler from '../src/Traveler.js'
-import sampleTravelerData from '..src/data/sampleData.js'
+import Traveler from '../src/Traveler'
+import sampleTravelerData from '..src/data/sampleData'
 
 // Add variables for sampleTravelerData here
 
 describe('Traveler', () => {
+  let traveler 
+
   beforeEach(() => {
-    Traveler = new Traveler(sampleTravelerData)
+    traveler = new Traveler(sampleTravelerData)
   })
 
 
