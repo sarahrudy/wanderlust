f6b59bf827607f7ffba1616d6bbda479aaf92331
Sarah <sarahrudy@gmail.com>
2021-08-09 18:16:35 -0600

    Add sampleData.js file to include smaller sample of data

---
+++ src/data/sampleData.js
@@ -0,0 +1,52 @@
+export const sampleTravelerData = [
+  {
+    id: 1,
+    name: "Ham Leadbeater",
+    travelerType: "relaxer"
+  },
+  {
+    id: 2,
+    name: "Rachael Vaughten",
+    travelerType: "thrill-seeker"
+  },
+  {
+    id: 3,
+    name: "Sibby Dawidowitsch",
+    travelerType: "shopper"
+  },
+]
+
+
+export const sampleTripsData = [
+  {
+    id: 1,
+    userID: 44,
+    destinationID: 49,
+    travelers: 1,
+    date: "2022/09/16",
+    duration: 8,
+    status: "approved",
+    suggestedActivities: []
+  },
+  {
+    id: 2,
+    userID: 35,
+    destinationID: 25,
+    travelers: 5,
+    date: "2022/10/04",
+    duration: 18,
+    status: "approved",
+    suggestedActivities: []
+  },
+  {
+    id: 3,
+    userID: 3,
+    destinationID: 22,
+    travelers: 4,
+    date: "2022/05/22",
+    duration: 17,
+    status: "approved",
+    suggestedActivities: []
+  },
+]
+
