b7d2bfbb8667223ded51314599cb3da49a5760bc
Sarah <sarahrudy@gmail.com>
2021-08-09 19:05:10 -0600

    Fix error message by adding curly brackets around import data

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,8 +1,7 @@
 import { expect } from 'chai';
 import Traveler from '../src/Traveler'
-import sampleTravelerData from '../src/data/sampleData'
+import { sampleTravelerData } from '../src/data/sampleData'
 
-// Add variables for sampleTravelerData here
 
 describe('Traveler', () => {
   let traveler 
