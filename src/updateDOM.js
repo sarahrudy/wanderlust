d3b49f45be316b83ff4e51231d99bca643b4c86c
Sarah <sarahrudy@gmail.com>
2021-08-10 00:15:16 -0600

    Add displayTripCards method to change innerHTML contents

--- src/updateDOM.js
+++ src/updateDOM.js
@@ -4,7 +4,41 @@ let updateDOM = {
     const greeting = document.querySelector("#greeting")
     const firstName = traveler.name.split(' ')[0]
     greeting.innerText = `Hi, ${firstName}!`
-  }
+  },
+
+  displayTripCards(trips) {
+    const tripsSection = document.querySelector('#tripsSection')
+    tripsSection.innerHTML = ''
+    trips.forEach(trip => {
+      tripsContainer.innerHTML +=
+      `<section class="trips-container" id=${trip.id}>
+        <div class="trips-container">
+          <article class="trip-cards">
+            <div class="destination-image" style=
+                "background: linear-gradient(rgba(0, 0, 0, 0.5), 
+                                              rgba(0, 0, 0, 0)),
+                 url(${trip.image}); 
+                 background-size: cover" 
+                 alt="${trip.alt}">
+            <div class="h3-container"> 
+              <h3 class="destination-name">${trip.destination}</h3>
+            </div>
+            <p>Travel Date: ${trip.date}</p>
+            <p>Number of People on Trip: ${trip.travelers}</p>
+            <p>Duration: ${trip.duration} days</p>
+            <p>Trip status: ${trip.status}</p>
+          </article>
+        </div>
+      </section >`
+    })
+  },
+
+
 }
+
+
+
+
+
 
 export default updateDOM
