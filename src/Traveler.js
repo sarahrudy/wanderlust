80ab000d8b246e674d037882be4232a9e663f643
Sarah <sarahrudy@gmail.com>
2021-08-10 12:45:07 -0600

    Add getMyTrips and getTripDetails methods to Traveler class to link dataSets

--- src/Traveler.js
+++ src/Traveler.js
@@ -5,6 +5,19 @@ class Traveler {
     this.travelerType = travelerData.travelerType;
     this.trips = []
   }
+
+  getMyTrips(tripsData) {
+    this.trips = tripsData.filter(trip => trip.userID === this.id);
+  }
+
+  getTripDetails(destinationData) {
+    this.trips.forEach(trip => {
+      let matchedDest = destinationData.find(dest => dest.id === trip.destinationID)
+      trip.destination = matchedDest.destination
+      trip.image = matchedDest.image
+      trip.alt = matchedDest.alt
+    });
+  }
 }
 
 export default Traveler 
