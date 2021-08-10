dd1906d2b9cf3bff95dffd346f67cf9ab5a76788
Sarah <sarahrudy@gmail.com>
2021-08-09 23:09:39 -0600

    Add dataSetter to add helper methods for getData function

--- src/scripts.js
+++ src/scripts.js
@@ -4,32 +4,42 @@
 import './css/base.scss'
 
 import {
-  getTravelerData,
   getTraveler,
-  getTripsData,
-  getDestinationsData
+  getAllTravelersData,
+  getAllTripsData,
+  getAllDestinationsData
 } from './API'
 
 import dayjs from 'dayjs'
 import Traveler from './Traveler'
 import TripsRepo from './TripsRepo'
+import updateDOM from './updateDOM'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-let allTravelers, traveler, trips, destinations
+let traveler, allTravelers, allTrips, allDestinations
 
-window.addEventListener('load', getData)
+window.addEventListener('load', getData(1))
 
-function getData() {
-  Promise.all([getTravelerData(), getTripsData(), getDestinationsData()])
+function getData(id) {
+  Promise.all([getTraveler(id), getAllTravelersData(), getAllTripsData(), getAllDestinationsData()])
     .then(data => {
-      // traveler = new Traveler(data[0])
-      allTravelers = data[0]
-      trips = data[1]
-      destinations = data[2]
+      dataSetter.setTraveler(data[0])
+      allTravelers = data[1]
+      allTrips = data[2]
+      allDestinations = data[3]
   })
 }
+
+let dataSetter = {
+  setTraveler(travelerData) {
+    traveler = new Traveler(travelerData)
+    updateDOM.greetUser(traveler)
+  }
+}
+
+
 
 
 
