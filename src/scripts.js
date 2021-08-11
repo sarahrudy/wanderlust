b12ad6bc49c7bdf3ed5cce6dbd901a8453ecf292
Sarah <sarahrudy@gmail.com>
2021-08-11 15:25:54 -0600

    Branch update for disable button bugfix

--- src/scripts.js
+++ src/scripts.js
@@ -14,10 +14,10 @@ import updateDOM from './updateDOM'
 
 // ------- QUERY SELECTORS --------
 const bookItButton = document.querySelector(".book-it-button")
-const destinationDropdown = document.querySelector("#destination")
-const numTravelers = document.querySelector("#num-travelers")
-const departDate = document.querySelector("#travel-date")
+const travelDate = document.querySelector("#travel-date")
 const tripDuration = document.querySelector("#trip-duration")
+const numTravelers = document.querySelector("#num-travelers")
+const destinationDropdown = document.querySelector("#destination")
 const tripCostLine = document.querySelector("#trip-cost")
 const calculateTripCostButton = document.querySelector(".calculate-cost-button")
 const loginSubmitButton = document.querySelector(".login-button")
@@ -25,10 +25,15 @@ const usernameInput = document.querySelector(".username-input")
 const passwordInput = document.querySelector(".password-input")
 
 
+
 // ------- EVENT LISTENERS --------
 bookItButton.addEventListener("click", bookTrip)
 calculateTripCostButton.addEventListener("click", calculateTripCost)
 loginSubmitButton.addEventListener("click", checkLogin)
+travelDate.addEventListener('change', buttonEnabler)
+tripDuration.addEventListener('change', buttonEnabler)
+numTravelers.addEventListener('change', buttonEnabler)
+
 
 
 window.addEventListener('load', getData(14))
@@ -46,7 +51,7 @@ function getData(id) {
       dataSetter.setTravelers(data[1])
       dataSetter.setTrips(data[2])
       dataSetter.setDestinations(data[3])
-      dataSetter.matchTripsToDestinations();
+      dataSetter.matchTripsToDestinations()
   })
 }
 
@@ -78,6 +83,32 @@ let dataSetter = {
   }
 }
 
+// ----- POST --------
+function postTripRequest() {
+  fetch("http://localhost:3001/api/v1/trips", {
+    method: "POST",
+    body: JSON.stringify({
+      "id": Date.now(),
+      "userID": parseInt(traveler.id),
+      "destinationID": parseInt(destinationDropdown.value),
+      "travelers": parseInt(numTravelers.value),
+      "date": formatDate(travelDate.value),
+      "duration": parseInt(tripDuration.value),
+      "status": "pending",
+      "suggestedActivities": []
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .then(data => traveler.trips.push(data))
+  .then(tripCostLine.innerText = "Trip request submitted!")
+  .catch(err => console.log(`POST Error: ${err.message}`))
+}
+
+
 // ---- PAGE FUNCTIONALITY ---------
 function getAnnualSpent() {
   const cost = traveler.calculateYearlySpent(traveler.trips)
@@ -90,32 +121,13 @@ function bookTrip() {
   setTimeout(function () { getData(traveler.id) }, 250)
 }
 
-// ----- POST --------
-function postTripRequest() {
-  fetch("http://localhost:3001/api/v1/trips", {
-    method: "POST",
-    body: JSON.stringify({
-      "id": Date.now(),
-      "userID": parseInt(traveler.id),
-      "destinationID": parseInt(destinationDropdown.value),
-      "travelers": parseInt(numTravelers.value),
-      "date": formatDate(departDate.value),
-      "duration": parseInt(tripDuration.value),
-      "status": "pending",
-      "suggestedActivities": []
-    }),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .then(data => traveler.trips.push(data))
-    .then(tripCostLine.innerText = "Trip request submitted!")
-    .catch(err => console.log(`POST Error: ${err.message}`))
+function buttonEnabler() {
+  if (travelDate.value && tripDuration.value && numTravelers.value) {
+    calculateTripCostButton.disabled = false
+    bookItButton.disabled = false
+  }
 }
 
-// ---- MORE PAGE FUNCTIONALITY -----
 function formatDate(dateValue) {
   let splitDate = dateValue.split("-")
   let joinedDate = splitDate.join("/")
