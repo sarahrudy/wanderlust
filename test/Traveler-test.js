8a9ac308c7a8cafb35a1928e75ace0c710024a20
Sarah <sarahrudy@gmail.com>
2021-08-11 09:44:00 -0600

    Test that traveler.getMyTrips will get trips that correspond with userID

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,13 +1,13 @@
 import { expect } from 'chai'
 import Traveler from '../src/Traveler'
 import { sampleTravelerData } from '../src/data/sampleData'
-
+import { sampleTripsData } from '../src/data/sampleData'
 
 describe('Traveler', () => {
   let traveler 
 
   beforeEach(() => {
-    traveler = new Traveler(sampleTravelerData[0])
+    traveler = new Traveler(sampleTravelerData[2])
   })
 
   it('should be a function', () => {
@@ -18,27 +18,22 @@ describe('Traveler', () => {
     expect(traveler).to.be.an.instanceOf(Traveler)  
   })
 
-  it('should be able to hold traveler data', () => {
-    expect(traveler.id).to.equal(1)
-    expect(traveler.name).to.equal('Ham Leadbeater')
-    expect(traveler.travelerType).to.equal('relaxer')
-  })
-
   it('should be able to get traveler by id', () => {
     traveler.getMyTrips(sampleTravelerData)
-    expect(traveler.id).to.deep.equal(1)
+    expect(traveler.id).to.deep.equal(3)
   })
 
   it('should instantiate with the traveler name', () => {
-    expect(traveler.name).to.equal("Ham Leadbeater")
+    expect(traveler.name).to.equal("Sibby Dawidowitsch")
   })
 
   it('should instantiate with a their travel type', () => {
-    expect(traveler.travelerType).to.equal("relaxer")
+    expect(traveler.travelerType).to.equal('shopper')
   })
 
   it("should be able to get trips that match ID", () => {
-    
+    traveler.getMyTrips(sampleTravelerData)
+    expect(traveler.trips[0]).to.deep.equal(sampleTripsData[3])
   })
 
 })
