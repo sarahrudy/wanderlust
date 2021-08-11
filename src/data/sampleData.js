1e15aafe01ab0aadee8374afaca812947c85dd88
Sarah <sarahrudy@gmail.com>
2021-08-11 10:18:05 -0600

    Add more sample data from datasets for testing

--- src/data/sampleData.js
+++ src/data/sampleData.js
@@ -14,6 +14,41 @@ export const sampleTravelerData = [
     name: "Sibby Dawidowitsch",
     travelerType: "shopper"
   },
+  {
+    id: 4,
+    name: "Leila Thebeaud",
+    travelerType: "photographer"
+  },
+  {
+    id: 5,
+    name: "Tiffy Grout",
+    travelerType: "thrill-seeker"
+  },
+  {
+    id: 6,
+    name: "Laverna Flawith",
+    travelerType: "shopper"
+  },
+  {
+    id: 7,
+    name: "Emmet Sandham",
+    travelerType: "relaxer"
+  },
+  {
+    id: 8,
+    name: "Carlin O'Reilly",
+    travelerType: "history buff"
+  },
+  {
+    id: 9,
+    name: "Natalee Deegin",
+    travelerType: "relaxer"
+  },
+  {
+    id: 10,
+    name: "Rickie Jodlowski",
+    travelerType: "relaxer"
+  },
 ]
 
 
@@ -48,6 +83,147 @@ export const sampleTripsData = [
     status: "approved",
     suggestedActivities: []
   },
+  {
+    id: 138,
+    userID: 25,
+    destinationID: 22,
+    travelers: 3,
+    date: "2020/10/29",
+    duration: 18,
+    status: "pending",
+    suggestedActivities: []
+  },
+  {
+    id: 139,
+    userID: 13,
+    destinationID: 7,
+    travelers: 5,
+    date: "2020/05/22",
+    duration: 16,
+    status: "approved",
+    suggestedActivities: []
+  },
+  {
+    id: 140,
+    userID: 46,
+    destinationID: 37,
+    travelers: 4,
+    date: "2021/01/18",
+    duration: 16,
+    status: "approved",
+    suggestedActivities: []
+  },
+  {
+    id: 141,
+    userID: 36,
+    destinationID: 19,
+    travelers: 5,
+    date: "2020/01/05",
+    duration: 18,
+    status: "approved",
+    suggestedActivities: []
+  },
+  {
+    id: 142,
+    userID: 42,
+    destinationID: 39,
+    travelers: 1,
+    date: "2019/12/14",
+    duration: 20,
+    status: "approved",
+    suggestedActivities: []
+  },
+  {
+    id: 143,
+    userID: 8,
+    destinationID: 7,
+    travelers: 3,
+    date: "2020/02/25",
+    duration: 12,
+    status: "approved",
+    suggestedActivities: []
+  },
+  {
+    id: 144,
+    userID: 45,
+    destinationID: 13,
+    travelers: 2,
+    date: "2020/09/21",
+    duration: 4,
+    status: "approved",
+    suggestedActivities: []
+  },
+  {
+    id: 145,
+    userID: 7,
+    destinationID: 31,
+    travelers: 6,
+    date: "2020/04/01/",
+    duration: 13,
+    status: "approved",
+    suggestedActivities: []
+  },
+  {
+    id: 146,
+    userID: 44,
+    destinationID: 31,
+    travelers: 1,
+    date: "2020/02/05",
+    duration: 12,
+    status: "approved",
+    suggestedActivities: []
+  },
+  {
+    id: 147,
+    userID: 8,
+    destinationID: 6,
+    travelers: 4,
+    date: "2019/09/18",
+    duration: 18,
+    status: "approved",
+    suggestedActivities: []
+  },
+  {
+    id: 148,
+    userID: 36,
+    destinationID: 30,
+    travelers: 1,
+    date: "2020/06/16",
+    duration: 19,
+    status: "approved",
+    suggestedActivities: []
+  },
+  {
+    id: 149,
+    userID: 18,
+    destinationID: 22,
+    travelers: 4,
+    date: "2019/10/01",
+    duration: 6,
+    status: "approved",
+    suggestedActivities: []
+  },
+  {
+    id: 150,
+    userID: 11,
+    destinationID: 20,
+    travelers: 5,
+    date: "2020/10/29",
+    duration: 8,
+    status: "approved",
+    suggestedActivities: []
+  },
+  {
+    id: 151,
+    userID: 46,
+    destinationID: 6,
+    travelers: 1,
+    date: "2020/02/11",
+    duration: 5,
+    status: "approved",
+    suggestedActivities: []
+  },
+  
 ]
 
 export const sampleDestinationsData = [
@@ -74,6 +250,46 @@ export const sampleDestinationsData = [
     estimatedFlightCostPerPerson: 950,
     image: "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
     alt: "opera house and city buildings on the water with boats"
+  },
+  {
+    id: 19,
+    destination: "Quito, Ecuador",
+    estimatedLodgingCostPerDay: 60,
+    estimatedFlightCostPerPerson: 500,
+    image: "https://images.unsplash.com/photo-1501684691657-cf3012635478?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+    alt: "a city at night with cloudy, snowy mountains in the distance"
+  },
+  {
+    id: 20,
+    destination: "Miami, Florida",
+    estimatedLodgingCostPerDay: 158,
+    estimatedFlightCostPerPerson: 275,
+    image: "https://images.unsplash.com/photo-1514214246283-d427a95c5d2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1573&q=80",
+    alt: "sand with palm trees and tall buildings in the background"
+  },
+  {
+    id: 21,
+    destination: "Tulum, Mexico",
+    estimatedLodgingCostPerDay: 100,
+    estimatedFlightCostPerPerson: 350,
+    image: "https://images.unsplash.com/photo-1501619593928-bef49688c888?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    alt: "a donkey standing on the beach"
+  },
+  {
+    id: 22,
+    destination: "Rome, Italy",
+    estimatedLodgingCostPerDay: 90,
+    estimatedFlightCostPerPerson: 650,
+    image: "https://images.unsplash.com/photo-1515542622106-78bda8ba0e5b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    alt: "people standing inside a colosseum during the day"
+  },
+  {
+    id: 23,
+    destination: "Copenhagen, Denmark",
+    estimatedLodgingCostPerDay: 120,
+    estimatedFlightCostPerPerson: 1000,
+    image: "https://images.unsplash.com/photo-1513622470522-26c3c8a854bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    alt: "colorful buildings with holiday decorations by the water with tents and docked boats"
   },
 ]
 
