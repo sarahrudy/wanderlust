e025ea7b7897ec55a9ec185ad0385e5b64ed4aa2
Sarah <sarahrudy@gmail.com>
2021-08-11 18:44:25 -0600

    Separate tests out by function

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -25,38 +25,48 @@ describe('Traveler', () => {
     expect(traveler).to.be.an.instanceOf(Traveler)  
   })
 
-  it('should instantiate with an id', () => {
-    expect(traveler.id).to.deep.equal(14)
+  describe('traveler properties', () => {
+    it('should instantiate with an id', () => {
+      expect(traveler.id).to.deep.equal(14)
+    })
+
+    it('should instantiate with a name', () => {
+      expect(traveler.name).to.deep.equal('Ellynn Kyne')
+    })
+
+    it('should instantiate with a traveler type', () => {
+      expect(traveler.travelerType).to.deep.equal('history buff')
+    })
+
+    it('should start off with an empty array of trips', () => {
+      expect(traveler.trips).to.deep.equal([])
+    })
   })
 
-  it('should instantiate with a name', () => {
-    expect(traveler.name).to.deep.equal('Ellynn Kyne')
+  describe('getMyTrips', () => {
+    it('should be able to get trips that match traveler id', () => {
+      traveler.getMyTrips(sampleTrips)
+      expect(traveler.trips[0]).to.deep.equal(sampleTrips[1])
+    })
+
+    it('should be able to count how many trips a traveler has', () => {
+      traveler.getMyTrips(sampleTrips)
+      expect(traveler.trips.length).to.equal(2)
+    })
+
+    it('should be able to return trip details for that traveler only', () => {
+      traveler.getMyTrips(sampleTrips)
+      expect(traveler.trips[0].userID).to.equal(14)
+      expect(traveler.trips[0].travelers).to.equal(1)
+      expect(traveler.trips[0].date).to.equal("2022/02/12")
+    })
   })
 
-  it('should instantiate with a traveler type', () => {
-    expect(traveler.travelerType).to.deep.equal('history buff')
-  })
 
-  it('should start off with an empty array of trips', () => {
-    expect(traveler.trips).to.deep.equal([])
-  })
 
-  it('should be able to get trips that match traveler id', () => {
-    traveler.getMyTrips(sampleTrips)
-    expect(traveler.trips[0]).to.deep.equal(sampleTrips[1])
-  })
 
-  it('should be able to count how many trips a traveler has', () => {
-    traveler.getMyTrips(sampleTrips)
-    expect(traveler.trips.length).to.equal(2)
-  })
 
-  it('should be able to return trip details for that traveler only', () => {
-    traveler.getMyTrips(sampleTrips)
-    expect(traveler.trips[0].userID).to.equal(14)
-    expect(traveler.trips[0].travelers).to.equal(1)
-    expect(traveler.trips[0].date).to.equal("2022/02/12")
-  })
 
+ 
 
 })
